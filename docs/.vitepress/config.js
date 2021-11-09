2c394907cb6bc80d15952afbaecf78e452b00501
落小梅 <425605679@qq.com>
2021-11-09 22:18:46 +0800

    docs: update vitepress config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,15 +18,43 @@ module.exports = {
   themeConfig: {
     //   头部导航
     nav: [
-      { text: '首页', link: '/' },
+      // { text: '首页', link: '/' },
+      { text: 'Guide', link: '/', activeMatch: '^/$|^/components/' },
       { text: '关于', link: '/about/' },
     ],
     //   侧边导航
-    sidebar: [
-      { text: '组件', link: '/components/', children: [
-          { text: 'button', link: 'button.md' },
-          { text: 'form', link: 'form.md' },
-        ] },
-    ]
+    sidebar: {
+      '/components/': getGuideSidebar(),
+      // '/config/': getConfigSidebar(),
+      '/': getGuideSidebar()
+    }
   }
+}
+function getGuideSidebar() {
+  return [
+    {
+      text: 'Introduction',
+      children: [
+        { text: 'What is Vben Components?', link: '/' },
+        { text: 'Getting Started', link: '/components/button' },
+        { text: 'Configuration', link: '/components/form' },
+        // { text: 'Asset Handling', link: '/guide/assets' },
+        // { text: 'Markdown Extensions', link: '/guide/markdown' },
+        // { text: 'Using Vue in Markdown', link: '/guide/using-vue' },
+        // { text: 'Deploying', link: '/guide/deploy' }
+      ]
+    },
+    // {
+    //   text: 'Advanced',
+    //   children: [
+    //     { text: 'Frontmatter', link: '/guide/frontmatter' },
+    //     { text: 'Theming', link: '/guide/theming' },
+    //     { text: 'API Reference', link: '/guide/api' },
+    //     {
+    //       text: 'Differences from Vuepress',
+    //       link: '/guide/differences-from-vuepress'
+    //     }
+    //   ]
+    // }
+  ]
 }
